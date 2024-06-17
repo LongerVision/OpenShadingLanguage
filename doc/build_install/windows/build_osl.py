e5e5da798b7049f01b333dc0933f622c82b79096
Pei Jia <jiapei@longervision.com>
2024-06-17 01:13:01 -0700

    Use C++17 as default, rather than C++14

--- doc/build_install/windows/build_osl.py
+++ doc/build_install/windows/build_osl.py
@@ -1049,7 +1049,7 @@ def InstallOSL(context, force, buildArgs):
 
         extraArgs.append("-DOSL_BUILD_TESTS=0")
         # if you are using LLVM 10 or higher C++ should be set on 14
-        extraArgs.append("-DCMAKE_CXX_STANDARD=14")
+        extraArgs.append("-DCMAKE_CXX_STANDARD=17")
 
         # if you are using LLVM 10 or higher C++ should be set on 11
         # extraArgs.append("-DCMAKE_CXX_STANDARD=11")
@@ -1273,7 +1273,7 @@ Users may specify custom build arguments for libraries using the --build-args
 option. This values for this option must take the form <library name>,<option>.
 For example:
 
-%(prog)s --build-args cxxflags=... OSL,-DCMAKE_CXX_STANDARD=14 ...
+%(prog)s --build-args cxxflags=... OSL,-DCMAKE_CXX_STANDARD=17 ...
 
 These arguments will be passed directly to the build system for the specified
 library. Multiple quotes may be needed to ensure arguments are passed on
